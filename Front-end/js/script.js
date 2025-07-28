53e729e1221daa8b811ecb1df6776b484380ab0e
davicouto530 <coutodavi530@gmail.com>
2025-07-28 14:56:16 -0300

    Implementando o gitignore

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -1,55 +1,59 @@
 let menuIcon = document.querySelector('#menu-icon');
 let ul = document.querySelector('.nav-list');
 let ip = "http://10.26.45.42:5000/api/v1";
+let usuario = "http://10.26.45.42:5000/api/v1/usuario/cadastrar"
 
 menuIcon.addEventListener('click', () => {
-    if (ul.classList.contains('ativo')) {
-        ul.classList.remove('ativo');
-        document.querySelector('#menu-icon img').src = 'img/burger-bar.png'
-    } else {
-        ul.classList.add('ativo');
-        document.querySelector('#menu-icon img').src = 'img/close.png';
-    }
+  if (ul.classList.contains('ativo')) {
+    ul.classList.remove('ativo');
+    document.querySelector('#menu-icon img').src = 'img/burger-bar.png'
+  } else {
+    ul.classList.add('ativo');
+    document.querySelector('#menu-icon img').src = 'img/close.png';
+  }
 })
 
 //-- Pegando dados do HTML 
 
 function coletarDados(event) {
-    event.preventDefault(); // Impede o reload da página
-    const nome = document.getElementById('txtNome').value;
-    const idade = document.getElementById('txtIdade').value;
-    const peso = document.getElementById('txtPeso').value;
-    const altura = document.getElementById('txtAltura').value;
-    const objetivo = document.getElementById('atividade').value;
-    const sexo = pegarSexoSelecionado();
+  fetch("http://10.26.45.42:5000/api/v1/usuario/cadastrar", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      nome,
+      idade,
+      sexo,
+      peso,
+      altura,
+      objetivo
+    })
+  })
+    .then((res) => {
+      if (!res.ok) throw new Error("Erro ao cadastrar usuário");
+      return res.json();
+    })
+    .then((resposta) => {
+      console.log("Usuário cadastrado:", resposta);
+      window.location.href = "perguntas.html";
+    })
+    .catch((err) => console.error(err));
 
-    console.log("Nome:", nome);
-    console.log("Idade:", idade);
-    console.log("Peso:", peso);
-    console.log("Objetivo:", objetivo);
-    console.log("Sexo:", sexo);
-
-    localStorage.setItem("nome", nome);
-    localStorage.setItem("idade", idade);
-    localStorage.setItem("peso", peso);
-    localStorage.setItem("altura", altura);
-    localStorage.setItem("objetivo", objetivo);
-    localStorage.setItem("sexo", sexo);
-
-    // Redireciona para a próxima página
-    window.location.href = "perguntas.html";
+  // Redireciona para a próxima página
+  window.location.href = "perguntas.html";
 }
 
 function pegarSexoSelecionado() {
-    const sexo = document.querySelector('input[name="grupo"]:checked');
+  const sexo = document.querySelector('input[name="grupo"]:checked');
 
-    if (sexo) {
-        console.log("Sexo selecionado:", sexo.value); // masculino ou feminino
-        return sexo.value;
-    } else {
-        console.log("Nenhum sexo selecionado");
-        return null;
-    }
+  if (sexo) {
+    console.log("Sexo selecionado:", sexo.value); // masculino ou feminino
+    return sexo.value;
+  } else {
+    console.log("Nenhum sexo selecionado");
+    return null;
+  }
 }
 
 // -------------------
