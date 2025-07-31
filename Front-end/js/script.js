8fac4f83e040fe3ca2f9fb3a426c9fc9c775375c
davicouto530 <coutodavi530@gmail.com>
2025-07-31 17:30:52 -0300

    Iniciando o fecth para a página de resposta

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -89,145 +89,6 @@ if (formHabitos) {
         const gordura = pegarResposta('gordura');
         const fumar = pegarResposta('fumar');
 
-        function pontuarResposta(pergunta, resposta) {
-            const tabela = {
-                perder_peso: {
-                    agua: {
-                        "Menos de 1 litro": 5,
-                        "1 - 2 litros": 10,
-                        "2 - 3 litros": 15,
-                        "Mais 3 litros": 20
-                    },
-                    "exercicios-dias": {
-                        "1 - 2 dias": 5,
-                        "3 - 5 dias": 10,
-                        "5 - 7 dias": 15
-                    },
-                    "exercicios-duracao": {
-                        "Menos de 30 minutos": 5,
-                        "30 a 60 minutos": 10,
-                        "Mais de 1 hora": 15
-                    },
-                    dormir: {
-                        "Menos de 5hr": 5,
-                        "5 - 7h": 10,
-                        "7 - 8h": 15,
-                        "Mais de 8h": 10
-                    },
-                    atividade: {
-                        "Sedentario": 0,
-                        "Moderado": 5,
-                        "Ativo": 10
-                    },
-                    frutas: {
-                        "Nunca": 0,
-                        "As vezes": 5,
-                        "Sempre": 10
-                    },
-                    gordura: {
-                        "Sim, todos os dias": 0,
-                        "As vezes": 5,
-                        "Raramente": 10
-                    },
-                    fumar: {
-                        "Sim": 0,
-                        "Raramente": 5,
-                        "Não": 10
-                    }
-                },
-                ganhar_massa: {
-                    agua: {
-                        "Menos de 1 litro": 0,
-                        "1 - 2 litros": 5,
-                        "2 - 3 litros": 10,
-                        "Mais 3 litros": 15
-                    },
-                    "exercicios-dias": {
-                        "1 - 2 dias": 0,
-                        "3 - 5 dias": 5,
-                        "5 - 7 dias": 15
-                    },
-                    "exercicios-duracao": {
-                        "Menos de 30 minutos": 0,
-                        "30 a 60 minutos": 5,
-                        "Mais de 1 hora": 15
-                    },
-                    dormir: {
-                        "Menos de 5hr": 0,
-                        "5 - 7h": 5,
-                        "7 - 8h": 15,
-                        "Mais de 8h": 10
-                    },
-                    atividade: {
-                        "Sedentario": 0,
-                        "Moderado": 5,
-                        "Ativo": 10
-                    },
-                    frutas: {
-                        "Nunca": 0,
-                        "As vezes": 5,
-                        "Sempre": 10
-                    },
-                    gordura: {
-                        "Sim, todos os dias": 10,
-                        "As vezes": 5,
-                        "Raramente": 0
-                    },
-                    fumar: {
-                        "Sim": 0,
-                        "Raramente": 5,
-                        "Não": 10
-                    }
-                },
-                manter_peso: {
-                    agua: {
-                        "Menos de 1 litro": 0,
-                        "1 - 2 litros": 10,
-                        "2 - 3 litros": 12,
-                        "Mais 3 litros": 15
-                    },
-                    "exercicios-dias": {
-                        "1 - 2 dias": 5,
-                        "3 - 5 dias": 10,
-                        "5 - 7 dias": 12
-                    },
-                    "exercicios-duracao": {
-                        "Menos de 30 minutos": 5,
-                        "30 a 60 minutos": 10,
-                        "Mais de 1 hora": 12
-                    },
-                    dormir: {
-                        "Menos de 5hr": 5,
-                        "5 - 7h": 10,
-                        "7 - 8h": 12,
-                        "Mais de 8h": 10
-                    },
-                    atividade: {
-                        "Sedentario": 0,
-                        "Moderado": 5,
-                        "Ativo": 10
-                    },
-                    frutas: {
-                        "Nunca": 0,
-                        "As vezes": 5,
-                        "Sempre": 10
-                    },
-                    gordura: {
-                        "Sim, todos os dias": 0,
-                        "As vezes": 5,
-                        "Raramente": 10
-                    },
-                    fumar: {
-                        "Sim": 0,
-                        "Raramente": 5,
-                        "Não": 10
-                    }
-                }
-            };
-
-            return tabela[objetivo]?.[pergunta]?.[resposta] || 0;
-        }
-
         const pontuacoesIndividuais = {
             agua: pontuarResposta('agua', agua),
             exerciciosDias: pontuarResposta('exercicios-dias', exerciciosDias),
@@ -254,7 +115,34 @@ if (formHabitos) {
             fumar
         }));
 
-        window.location.href = "grafico.html";
+        // ✅ Aqui inicia o envio ao backend
+        fetch("http://10.26.45.42:5000/api/v1/resposta/cadastro", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                id_usuario,
+                agua,
+                exercicio_semanal,
+                tempo_treino,
+                sono,
+                atividade_diaria,
+                frutas_vegetais,
+                doces_fritura,
+                fuma_bebe,
+                pontuacao
+            })
+        })
+            .then(res => {
+                if (!res.ok) throw new Error(`Erro ${res.status} ao salvar hábitos`);
+                return res.json();
+            })
+            .then(data => {
+                console.log("Respostas cadastradas:", data);
+                window.location.href = "grafico.html";
+            })
+            .catch(err => {
+                console.error("Falha ao enviar:", err);
+            });
     });
 }
 
